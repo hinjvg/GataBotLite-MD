ed435821ad70c0f3ad5bfe59200c853ae8cb282d
hinjvg <atatt7205@gmail.com>
2025-03-05 21:28:23 +0200

    Update _language-GB.js

--- plugins/_language-GB.js
+++ plugins/_language-GB.js
@@ -5,21 +5,9 @@ global.wait2 = lenguajeGB.smsWait()
   
 let user = global.db.data.users[m.sender]
 
-if (user.GBLanguage == 'es') {
-global.lenguajeGB = es
-} else if (user.GBLanguage == 'en') {
-global.lenguajeGB = en
-} else if (user.GBLanguage == 'pt') {
-global.lenguajeGB = pt
+
 } else if (user.GBLanguage == 'ar') {
 global.lenguajeGB = ar
-} else if (user.GBLanguage == 'id') {
-global.lenguajeGB = id
-} else if (user.GBLanguage == 'de') {
-global.lenguajeGB = de
-} else if (user.GBLanguage == 'it') {
-global.lenguajeGB = it
-} else {
-global.lenguajeGB = es
+} 
 }
 }
